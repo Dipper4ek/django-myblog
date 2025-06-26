8404ea5520a86a65e6ebc6ebf223a41740172fd0
Dipper4ek <itworkerkmykola@gmail.com>
2025-06-26 18:20:52 +0200

    updated

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField()
+    published_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+    def published_recently(self):
+        return self.published_date >= timezone.now() - timedelta(days=7)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Comment by {self.author_name}'
